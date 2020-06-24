5fb6f789841088b6987c512e8e7e3104facb5ef2
Nicholas West <nwest13@vols.utk.edu>
2020-06-24 13:20:37 -0400

    Added test_all.sh

---
+++ test/test_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+sh test_matlab_broadcast.sh
+sh test_matlab_devices.sh
+sh test_matlab_send.sh
+sh test_matlab_sync.sh
+sh test_matlab_tcp.sh
+sh test_python_broadcast.sh
+sh test_python_devices.sh
+sh test_python_send.sh
+sh test_python_sync.sh
+sh test_python_tcp.sh
+sh test_matlab_python_interop.sh
