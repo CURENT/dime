13811151c7b026abe3170a0f34fa8bfbb280a5ed
Nicholas West <nwest13@vols.utk.edu>
2020-06-24 20:29:59 -0400

    Changed default behavior from crashing to printing error

--- server/server.c
+++ server/server.c
@@ -20,6 +20,64 @@
 #include "socket.h"
 #include <stdio.h>
 
+#if !(defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__))
+/* Shamelessly stolen from OpenBSD */
+
+static size_t strlcpy(char *restrict dst, const char *restrict src, size_t dsize) {
+    const char *osrc = src;
+    size_t nleft = dsize;
+
+    /* Copy as many bytes as will fit. */
+    if (nleft != 0) {
+        while (--nleft != 0) {
+            if ((*dst++ = *src++) == '\0') {
+                break;
+            }
+        }
+    }
+
+    /* Not enough room in dst, add NUL and traverse rest of src. */
+    if (nleft == 0) {
+        if (dsize != 0) {
+            *dst = '\0'; /* NUL-terminate dst */
+        }
+
+        while (*src++);
+    }
+
+    return src - osrc - 1; /* count does not include NUL */
+}
+
+static size_t strlcat(char *dst, const char *src, size_t dsize) {
+    const char *odst = dst;
+    const char *osrc = src;
+    size_t n = dsize;
+    size_t dlen;
+
+    /* Find the end of dst and adjust bytes left but don't go past end. */
+    while (n-- != 0 && *dst != '\0') {
+        dst++;
+    }
+    dlen = dst - odst;
+    n = dsize - dlen;
+
+    if (n-- == 0) {
+        return dlen + strlen(src);
+    }
+    while (*src != '\0') {
+        if (n != 0) {
+            *dst++ = *src;
+            n--;
+        }
+        src++;
+    }
+    *dst = '\0';
+
+    return dlen + (src - osrc); /* count does not include NUL */
+}
+
+#endif
+
 static int cmp_fd(const void *a, const void *b) {
     return (*(const int *)b) - (*(const int *)a);
 }
@@ -390,26 +448,23 @@ int dime_server_loop(dime_server_t *srv) {
                         } else if (strcmp(cmd, "devices") == 0) {
                             err = dime_client_devices(clnt, srv, jsondata, &bindata, bindata_len);
                         } else {
-                            json_decref(jsondata);
-                            free(bindata);
-                            signal(SIGPIPE, sigpipe_f);
-                            signal(SIGTERM, sigterm_f);
-                            signal(SIGINT, sigint_f);
-                            free(pollfds);
+                            err = -1;
 
-                            printf("%d\n", __LINE__); return -1;
+                            strlcpy(srv->err, "Unknown command: ", sizeof(srv->err));
+                            strlcat(srv->err, cmd, sizeof(srv->err));
+
+                            json_t *response = json_pack("{sisss+}", "status", -1, "error", "Unknown command: ", cmd);
+                            if (response != NULL) {
+                                dime_socket_push(&clnt->sock, response, NULL, 0);
+                                json_decref(response);
+                            }
                         }
 
                         json_decref(jsondata);
                         free(bindata);
 
                         if (err < 0) {
-                            signal(SIGPIPE, sigpipe_f);
-                            signal(SIGTERM, sigterm_f);
-                            signal(SIGINT, sigint_f);
-                            free(pollfds);
-
-                            printf("%d\n", __LINE__); return -1;
+                            fprintf(stderr, "Error: %s\n", srv->err);
                         }
                     } else if (n < 0) {
                         signal(SIGPIPE, sigpipe_f);
