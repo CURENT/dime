6ea9285e53bf5bd8d7648764675e5b30ec432623
Nicholas West <nwest13@vols.utk.edu>
2021-10-27 10:57:58 -0400

    updated config.mk for Windows
    Former-commit-id: d96848d812db96012578d4bd8c2f034c03921448

--- server/config.mk
+++ server/config.mk
@@ -1,20 +1,20 @@
 # C compiler on your system, "cc" or "gcc" are usually good choices
-CC ?= cc
+CC := cc
 
 # "make" command, usually just "make"
-MAKE ?= make
+MAKE := make
 
 # Installation directory
-PREFIX ?= /usr/local
+PREFIX := /usr/local
 
 # Man pages directory
-MANDIR ?= ${PREFIX}/share/man
+MANDIR := ${PREFIX}/share/man
 
 # C compilation flags
-CFLAGS += -std=c99 -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE=500 -Wall -pedantic -Wno-error -fPIE -pthread
+CFLAGS := ${CFLAGS} -std=c99 -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE=500 -Wall -pedantic -Wno-error -fPIE -fno-strict-aliasing -pthread
 
 # C linker flags
-LDFLAGS += -pie -pthread
+LDFLAGS := ${LDFLAGS} -pie -pthread
 
 # Uncomment the lines below for a release build
 #CFLAGS += -DNDEBUG -O3
@@ -25,3 +25,7 @@ CFLAGS += -Og -g -fstack-protector-strong
 # Uncomment the lines below to enable AddressSanitizer (Valgrind will not work)
 #CFLAGS += -fsanitize=address
 #LDFLAGS += -fsanitize=address
+
+# Uncomment the lines below to compile for Windows
+#CC := x86_64-w64-mingw32-gcc
+#LDFLAGS += -lws2_32
