775492d433ac7051f4b862e30b62b8ad858e24fa
jinningwang <jinningwang@users.noreply.github.com>
2023-04-03 14:43:03 -0400

    [WIP] Pack

--- server/config.mk
+++ server/config.mk
@@ -1,20 +1,26 @@
 # C compiler on your system, "cc" or "gcc" are usually good choices
-CC := cc
+CC := gcc
 
 # "make" command, usually just "make"
 MAKE := make
 
 # Installation directory
-PREFIX := /usr/local
+PREFIX := $(shell conda info --base)/envs/$(shell conda env list | grep "*" | cut -f1 -d ' ')
+
+JANSSON_INC := $(PREFIX)/include
+JANSSON_LIB := $(PREFIX)/lib
 
 # Man pages directory
 MANDIR := ${PREFIX}/share/man
 
 # C compilation flags
-CFLAGS := ${CFLAGS} -std=c99 -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE=500 -Wall -pedantic -Wno-error -fPIE -fno-strict-aliasing -pthread
+CFLAGS := ${CFLAGS} -I$(JANSSON_INC) -std=c99 -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE=500 -Wall -pedantic -Wno-error -fPIE -fno-strict-aliasing -pthread
+# CFLAGS := ${CFLAGS} -I${PREFIX}/include
 
 # C linker flags
-LDFLAGS := ${LDFLAGS} -pie -pthread
+LDFLAGS := ${LDFLAGS} -pie -pthread -L$(JANSSON_LIB)/lib
+# LD_LIBRARY_PATH = $(CONDA_PREFIX)/lib
+# LDFLAGS := ${LDFLAGS} -L${PREFIX}/lib -ljansson -lev -lssl -lcrypto -lz
 
 # Uncomment the lines below for a release build
 #CFLAGS += -DNDEBUG -O3
