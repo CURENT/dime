fbcc5a6c49e604d49d3e0f9a1c32d47e3f2a6505
zmalkmus <zmalkmus@vols.utk.edu>
2023-05-17 11:04:51 -0400

    added source suffix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,8 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -28,6 +28,8 @@ release = '2.0.0'
 # -- General configuration ---------------------------------------------------
 
 master_doc = 'index'
+
+source_suffix = ['.rst']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
