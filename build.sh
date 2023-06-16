7fdec4b84b64e0dd1cf0d0a4daf13c5e00faa616
zmalkmus <zmalkmus@vols.utk.edu>
2023-06-16 15:34:59 -0400

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ sudo apt-get install -y build-essential autotools-dev autoconf libev-dev libtool
 (cd openssl && ./config && make && sudo make install)
 
 # Build zlib
-(cd zlib && chmod +x configure && ./configure && make && sudo make install)
+(cd zlib-* && chmod +x configure && ./configure && make && sudo make install)
 
 # Build jansson
-(cd jansson && autoreconf -i --force && chmod +x configure && ./configure && make && sudo make install)
+(cd jansson-* && autoreconf -i --force && chmod +x configure && ./configure && make && sudo make install)
 
 # Build dime
-(cd dime/server && make && make install)
-(cd dime/client/python && $PYTHON setup.py install)
+(cd dime/server && make && sudo make install)
+(cd dime/client/python && python3 setup.py install)
