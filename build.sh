7fab0f966e5ff55c2ad44869045f55a4104f33a1
zmalkmus <zmalkmus@vols.utk.edu>
2023-07-20 13:21:28 -0400

    updated build.sh script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/usr/bin/sudo bash
+
 function start_spinner {
     set +m
     printf "%-36s" "    $1 "
@@ -26,36 +27,57 @@ printf "Building dime from source... This may take a while."
 # Install dependencies
 printf "\n"
 start_spinner "Installing dependencies:"
-sudo apt-get update &> /dev/null
-sudo apt-get upgrade -y &> /dev/null
-sudo apt-get install -y build-essential autotools-dev autoconf libev-dev libtool 1> /dev/null
-sudo apt-get install -y python3-pip 1> /dev/null
-sudo pip install setuptools
+# apt-get update &> /dev/null
+# apt-get upgrade -y &> /dev/null
+# apt-get install -y build-essential autotools-dev autoconf libev-dev libtool 1> /dev/null
+# apt-get install -y python3-pip 1> /dev/null
+# pip install setuptools
+
+(
+    apt update \
+    && apt install -y --no-install-recommends \
+        git \
+        build-essential \
+        libsuitesparse-dev \
+        libopenblas-dev \
+        libjansson-dev \
+        libssl-dev \
+        zlib1g-dev \
+        libev-dev \
+        autotools-dev \
+        autoconf \
+        libtool \
+        python3-pip \
+    && rm -rf /var/lib/apt/lists/*
+) &> /dev/null
+
+python3 -m pip install --upgrade pip setuptools wheel &> /dev/null
+
 stop_spinner
 echo "    Installing dependencies:      ✓"
 
-# Build openssl
-start_spinner "Building openssl:"
-(cd openssl* && sudo chmod +x config Configure && ./config && make && sudo make install) 1> /dev/null
-stop_spinner
-echo "    Building openssl:             ✓"
+# # Build openssl
+# start_spinner "Building openssl:"
+# (cd openssl* && chmod +x config Configure && ./config && make && make install) 1> /dev/null
+# stop_spinner
+# echo "    Building openssl:             ✓"
 
-# Build zlib
-start_spinner "Building zlib:"
-(cd zlib* && sudo chmod +x configure && ./configure && make && sudo make install) 1> /dev/null
-stop_spinner
-echo "    Building zlib:                ✓"
+# # Build zlib
+# start_spinner "Building zlib:"
+# (cd zlib* && chmod +x configure && ./configure && make && make install) 1> /dev/null
+# stop_spinner
+# echo "    Building zlib:                ✓"
 
-# Build jansson
-start_spinner "Building jansson:"
-(cd jansson* && sudo autoreconf --install --force && sudo chmod 777 configure && ./configure && make && sudo make install) #&> /dev/null
-stop_spinner
-echo "    Building jansson:             ✓"
+# # Build jansson
+# start_spinner "Building jansson:"
+# (cd jansson* && autoreconf --install --force && chmod 777 configure && ./configure && make && make install) #&> /dev/null
+# stop_spinner
+# echo "    Building jansson:             ✓"
 
 # Build dime
 start_spinner "Building dime:"
-(cd dime/server && make && sudo make install) #&> /dev/null
-(cd dime/client/python && python3 setup.py install) #&> /dev/null
+(cd dime/server && make && make install) &> /dev/null
+(cd dime/client/python && python3 setup.py install) &> /dev/null
 stop_spinner
 echo "    Building dime:                ✓"
 
